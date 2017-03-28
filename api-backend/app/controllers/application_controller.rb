41fc674c5801babb99dbf8626fabc024d008047d
dgwp-projects <zayneabraham1@gmail.com>
2017-03-28 16:02:22 -0400

    yelp working, giphy working

--- api-backend/app/controllers/application_controller.rb
+++ api-backend/app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+	def index
+
+		yelp_url = 'https://api.yelp.com/v3/businesses/search'
+		
+		
+		
+		term = params[:term]
+
+		location = params[:location]
+		final_url = yelp_url + "?term=" + term + "&location=" + location
+		
+		@api_data = JSON.parse(RestClient.get(final_url, headers={'Authorization': "Bearer nEiwOdSrYOjDZMs4GCyu84kffABwGtciSCqFpd7sbIHY69fK1Hdt_FZa1--8Up6wWALWzR_Z3hLSBJS0uWyzZT6ThWyefqU4EEWHY_mqbGpd7nsEPkeFqX-tXqLaWHYx"}).body)	
+		@api_data_json = @api_data.to_json
+		render 'api/data'
+		
+	end
 end
